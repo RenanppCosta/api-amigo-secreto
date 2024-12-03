ef52060c35d58e6779b0d4053482aba57fe6e7fd
Renanppcosta <renanppcosta@gmail.com>
2024-12-03 16:36:01 -0300

    feat: created DTO of participants and register and get participants

---
+++ src/dtos/participants.py
@@ -0,0 +1,5 @@
+from pydantic import BaseModel, EmailStr
+
+class ParticipantRegistrerDTO(BaseModel):
+    name: str
+    email: EmailStr
--- src/routes/participant.py
+++ src/routes/participant.py
@@ -1,4 +1,6 @@
 from fastapi import APIRouter
+from src.dtos.participants import ParticipantRegistrerDTO
+from src.models.participant import Participant
 
 router = APIRouter(
     prefix="/participant",
@@ -6,6 +8,15 @@ router = APIRouter(
     responses={404: {"description": "Not Found"}}
 )
 
+@router.post("/")
+async def register_participant(body: ParticipantRegistrerDTO):
+    participant = await Participant.create(
+        name = body.name,
+        email = body.email
+    )
+    return {"participant": participant}
+
 @router.get("/")
-async def home():
-    return {"status": "Ok"}
+async def get_participants():
+    participants = await Participant.all()
+    return {"participants": participants}
