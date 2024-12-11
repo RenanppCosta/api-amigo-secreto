d1cec7f2011722d4bffdb0459ff4127f7747121a
Renanppcosta <renanppcosta@gmail.com>
2024-12-11 17:55:10 -0300

    feat: added config of cors

--- src/main.py
+++ src/main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware 
 from src.db.config import config_db
 from src.routes.router import config_routes
 
 def create_app():
     app = FastAPI()
+
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["http://localhost:3000"],  
+        allow_credentials=True,
+        allow_methods=["*"],  
+        allow_headers=["*"],  
+    )
 
     config_db(app)
     config_routes(app)
@@ -11,4 +20,3 @@ def create_app():
     return app
 
 app = create_app()
-
