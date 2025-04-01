6730655a83a5ed0545fc27623920dbad7bf6659a
renanppcosta <renanppcosta@gmail.com>
2025-04-01 14:33:22 -0300

    feat: using middleware get_current_user in group router

--- src/routes/group.py
+++ src/routes/group.py
@@ -1,6 +1,10 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from src.dtos.groups import GroupRegistrerDTO
 from src.models.group import Group
+from src.models.user import User
+from src.models.participant import Participant
+from src.middlewares.auth import get_current_user
+
 router = APIRouter(
     prefix="/group",
     tags=["Groups"],
@@ -8,20 +12,30 @@ router = APIRouter(
 )
 
 @router.post("/")
-async def create_group(body: GroupRegistrerDTO):
-    group = await Group.create(
+async def create_group(body: GroupRegistrerDTO, user_logged: User = Depends(get_current_user)):
+
+    new_group = await Group.create(
+        created_by = user_logged,
         name = body.name,
         date = body.date
     )
+
+    await Participant.create(
+        name = user_logged.name,
+        email = user_logged.email,
+        group = new_group
+    )
+
     return {
-        "id": group.id,
-        "name": group.name,
-        "date": group.formatted_date
+        "id": new_group.id,
+        "name": new_group.name,
+        "date": new_group.formatted_date,
+        "created_by": new_group.created_by.name
     }
 
 @router.get("/")
-async def get_groups():
-    groups = await Group.all().prefetch_related("participants") 
+async def get_groups(user_logged: User = Depends(get_current_user)):
+    groups = await Group.all().prefetch_related("participants").filter(created_by=user_logged)
     return [
         {
             "id": group.id,
